f111dcf65f448aacc15eda102711f5bd06d94e46
marcuszuccaro <59516978+marcuszuccaro@users.noreply.github.com>
2023-02-24 21:02:16 -0500

    Wrote autons in pathplanner and tried writing the part in autons for them look at comments for them in Autos.java

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,8 +4,13 @@
 
 package frc.robot.commands;
 
+import frc.robot.Constants.ELEVATOR.Height;
 import frc.robot.Path;
+import frc.robot.subsystems.Carriage;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.SwerveDrive;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
@@ -13,6 +18,31 @@ public final class Autos {
   /** Example static factory for an autonomous command. */
   public static CommandBase driveForward(SwerveDrive swerve) {
     return Commands.sequence(swerve.driveOnPath(Path.Drive_Forward_1));
+  }
+
+  // public static CommandBase farLeftPlace2(SwerveDrive swerve, Intake intake,
+  // Elevator elevator, Carriage carriage) {
+  // return Commands.sequence(elevator.goToDesiredHeight(Height.HIGH),
+  // (carriage.placeElement()),
+  // (swerve.driveOnPath(Path.Far_Left_Path_Place2)), (intake.runIntakeAuton()),
+  // (elevator.goToDesiredHeight(Height.HIGH)), (carriage.placeElement()));//TODO
+  // Figure out how to have swerve and intake run in parrallel
+  // } This was my first attempt and the one betlow is second attempt
+
+  public static CommandBase farLeftPlace2(SwerveDrive swerve, Intake intake, Elevator elevator, Carriage carriage) {
+    return Commands.parallel(elevator.goToDesiredHeight(Height.HIGH).andThen(carriage.placeElement())
+        .andThen((swerve.driveOnPath(Path.Far_Left_Path_Place2)), (intake.runIntakeAuton()))
+        .andThen((elevator.goToDesiredHeight(Height.HIGH)).andThen((carriage.placeElement()))));
+  }
+
+  // TODO figure out how to write them if they need to be parallel or sequence
+  // because the swerve drive and intake need to be run parallel but the rest
+  // needs to be sequence. May need to make a new class for each auton
+  public static CommandBase farLeftPlace2PickUp(SwerveDrive swerve, Intake intake, Elevator elevator,
+      Carriage carriage) {
+    return Commands.sequence(elevator.goToDesiredHeight(Height.HIGH).andThen(carriage.placeElement())
+        .andThen(swerve.driveOnPath(Path.Far_Left_Path_Place2), intake.runIntakeAuton())
+        .andThen(elevator.goToDesiredHeight(Height.HIGH).andThen(carriage.placeElement())));
   }
 
   private Autos() {
