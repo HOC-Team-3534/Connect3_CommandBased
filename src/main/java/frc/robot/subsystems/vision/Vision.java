232d3cf95ea8a6672064cafe7d3a755e3e282c9c
Malachi Bergman <malachijb28@gmail.com>
2023-03-20 15:54:32 -0400

    add latency calculatino based on timestamp for debugging

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -69,6 +70,9 @@ public class Vision extends SubsystemBase {
             Logger.getInstance().recordOutput("Vision/Pose", visionData.pose);
             Logger.getInstance().recordOutput("Vision/Timestamp", visionData.estBotPoseTimestampSecs);
             Logger.getInstance().recordOutput("Vision/Latency", visionData.estBotPoseLatencySecs);
+
+            Logger.getInstance().recordOutput("Vision/Timestamp Based Latency Calculated",
+                    Timer.getFPGATimestamp() - visionData.estBotPoseTimestampSecs);
         }
     }
 
